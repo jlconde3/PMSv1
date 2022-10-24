a0ba41c073edf86a062527b063f1f22afa0215b1
Jose <jlconde3@gmail.com>
2022-10-24 17:18:06 +0200

    POPUP

---
+++ APP/static/scripts/tools/wp.js
@@ -0,0 +1,8 @@
+document.getElementById('more_button').addEventListener('click',() => {
+    
+});
+
+document.getElementById('generate_button').addEventListener('click',() => {
+    document.getElementById('main').setAttribute('style', 'filter:blur(3px)');
+    document.getElementById('validate-window').style.visibility = 'visible';
+});
