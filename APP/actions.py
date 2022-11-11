13f465a57cb0f0620df935097ee4e46bd0d716e0
Jose <jlconde3@gmail.com>
2022-11-11 09:41:45 +0100

    create folders for each tool

--- APP/actions.py
+++ APP/actions.py
@@ -1,11 +1,8 @@
-from hashlib import sha256
 from flask import render_template, Blueprint, request,g, make_response
-from auth import login_required
-from datetime import datetime
-from common import MySQLHelper, InputClass, CustomViews
+from auth import login_required, CustomViews
+from common import MySQLHelper, InputClass
 
+bp = Blueprint('actions', __name__, url_prefix='/actions')
 
-
-
-
-
+bp.add_url_rule('/create_action', view_func=CustomViews.as_view('/create_action','actions,','create_action'))
+bp.add_url_rule('/modify_action', view_func=CustomViews.as_view('/modify_action','actions','modify_action'))
