23ef52906ba417e5b7c692ac869fcfea6708c65f
Jose <jlconde3@gmail.com>
2022-12-07 14:35:23 +0100

    add user view

--- APP/user/user.py
+++ APP/user/user.py
@@ -2,11 +2,11 @@ from flask import render_template, Blueprint, request
 from auth.auth import login_required, CustomViews
 
 
-bp = Blueprint('user', __name__, url_prefix='/user')
+bp = Blueprint('user', __name__, url_prefix='/user', template_folder='templates')
 
 
-bp.add_url_rule('/', view_func=CustomViews.as_view('/', '/public/profile.html'))
-bp.add_url_rule('/settings', view_func=CustomViews.as_view('/settings','/public/settings.html'))
+bp.add_url_rule('/', view_func=CustomViews.as_view('/', 'user/profile.html'))
+
 
 
 
