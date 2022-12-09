0bcca1ee72710d271746df60297a830b0bc64400
Jose <jlconde3@gmail.com>
2022-12-09 10:53:54 +0100

    fix auth module

--- APP/auth/auth.py
+++ APP/auth/auth.py
@@ -9,6 +9,7 @@ from flask import render_template, Blueprint, redirect, url_for, request, sessio
 
 bp = Blueprint('auth', __name__, url_prefix='/auth', template_folder='templates', static_folder='static')
 
+
 def check_password_hash (user_password:str,input_password:str):
     if user_password == sha256(input_password.encode()).hexdigest():
         return True
@@ -18,8 +19,8 @@ def check_password_hash (user_password:str,input_password:str):
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login_user'))
+        if g.user_id is None:
+            return redirect(url_for('auth.login'))
         return view(**kwargs)
     return wrapped_view
 
@@ -74,6 +75,38 @@ def login_model (username, password):
 def logout_model():
     session['user_id'] = None
     session['user_rol'] = None
+
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        login_status = login_model(request.form['username'],request.form['password'])
+
+        if login_status[0]:
+            return redirect(url_for('index'))
+        return render_template('auth/login.html', error = login_status[1], user = login_status[2])
+
+    elif request.method == 'GET':
+        return render_template('auth/login.html')
+
+
+@bp.route('/logout', methods=('GET', 'POST'))
+def logout():
+    logout_model()
+    return redirect(url_for('auth.login'))
+
+
+@bp.before_app_request
+def load_logged_user ():
+    user_id = session.get('user_id')
+    user_rol = session.get('user_rol')
+    if  user_id is None:
+        g.user_id = None
+        g.user_rol = None
+    else: 
+        g.user_id = user_id
+        g.user_rol = user_rol
 
 
 
--- APP/auth/data.py
+++ APP/auth/data.py
@@ -7,12 +7,10 @@ def login_data(username):
     :param username: input vaule for user from form.
     """
     MySQL = MySQLHelper()
-    MySQL.cursor.execute('SELECT user,password FROM `pms-auth`.`users-login` WHERE user = %s ORDER BY id DESC LIMIT 1', (username,))
+    MySQL.cursor.execute('SELECT user,password FROM `pms-users`.`users-login` WHERE user = %s ORDER BY id DESC LIMIT 1', (username,))
     user = MySQL.cursor.fetchone()
     MySQL.con.close()
     return user
-
-
 
 def login_client(username):
     """
@@ -20,7 +18,7 @@ def login_client(username):
     :param username: input vaule for user from form.
     """
     MySQL = MySQLHelper()
-    MySQL.cursor.execute('SELECT client FROM `pms-auth`.`users-client` WHERE user = %s ORDER BY id DESC LIMIT 1', (username,))
+    MySQL.cursor.execute('SELECT client FROM `pms-users`.`users-client` WHERE user = %s ORDER BY id DESC LIMIT 1', (username,))
     client = MySQL.cursor.fetchone()
     MySQL.con.close()
     return client
@@ -33,7 +31,7 @@ def login_rol(username):
     :param username: input vaule for user from form.
     """
     MySQL = MySQLHelper()
-    MySQL.cursor.execute('SELECT rol FROM `pms-auth`.`users-rol` WHERE user = %s ORDER BY id DESC LIMIT 1', (username,))
+    MySQL.cursor.execute('SELECT rol FROM `pms-users`.`users-rol` WHERE user = %s ORDER BY id DESC LIMIT 1', (username,))
     rol = MySQL.cursor.fetchone()
     MySQL.con.close()
     return rol
